ebbee3df7c0d317dd6eaf8152ebee3227bf7a2d1
shme-e <143431274+shme-e@users.noreply.github.com>
2025-11-15 11:56:53 +0000

    increase player resilience

--- IntroBot3/Services/ThemePlayerService.cs
+++ IntroBot3/Services/ThemePlayerService.cs
@@ -42,91 +42,99 @@ public class ThemePlayerService(ILogger<ThemePlayerService> logger)
 
         while (_themeQueue.Count > 0)
         {
-            var theme = _themeQueue.Peek();
-
-            if (_stream == null || _voiceClient == null || _currentChannelId == null || _currentChannelId != theme.ChannelId)
+            try
             {
-                if (_voiceClient != null)
+                var theme = _themeQueue.Peek();
+
+                if (_stream == null || _voiceClient == null || _currentChannelId == null || _currentChannelId != theme.ChannelId)
+                {
+                    if (_voiceClient != null)
+                    {
+                        await _voiceClient.CloseAsync();
+                        _voiceClient.Dispose();
+                    }
+
+                    _stream?.Dispose();
+
+                    _voiceClient = await client.JoinVoiceChannelAsync(theme.GuildId, theme.ChannelId, new VoiceClientConfiguration
+                    {
+                        Logger = new ConsoleLogger()
+                    });
+                    _currentChannelId = theme.ChannelId;
+
+                    await Task.Delay(250);
+                    await _voiceClient.StartAsync();
+                    await Task.Delay(250);
+                    await _voiceClient.EnterSpeakingStateAsync(new SpeakingProperties(SpeakingFlags.Microphone));
+
+                    var outStream = _voiceClient.CreateOutputStream();
+
+                    _stream = new OpusEncodeStream(outStream, PcmFormat.Short, VoiceChannels.Stereo, OpusApplication.Audio);
+                }
+
+                ProcessStartInfo startInfo = new("ffmpeg")
+                {
+                    RedirectStandardOutput = true,
+                };
+                var arguments = startInfo.ArgumentList;
+
+                // Specify the input
+                arguments.Add("-i");
+                arguments.Add(theme.FilePath);
+
+                // Set the logging level to quiet mode
+                arguments.Add("-loglevel");
+                arguments.Add("-8");
+
+                // Set the number of audio channels to 2 (stereo)
+                arguments.Add("-ac");
+                arguments.Add("2");
+
+                // Set the output format to 16-bit signed little-endian
+                arguments.Add("-f");
+                arguments.Add("s16le");
+
+                // Set the audio sampling rate to 48 kHz
+                arguments.Add("-ar");
+                arguments.Add("48000");
+
+                // Direct the output to stdout
+                arguments.Add("pipe:1");
+
+                // Start the FFmpeg process
+                var ffmpeg = Process.Start(startInfo)!;
+
+                logger.LogInformation("Playing theme: {ThemeFilePath} in Guild: {GuildId}, Channel: {ChannelId}", theme.FilePath, theme.GuildId, theme.ChannelId);
+
+                await ffmpeg.StandardOutput.BaseStream.CopyToAsync(_stream);
+
+                await _stream.FlushAsync();
+
+                if (_themeQueue.Count == 1)
+                {
+                    await Task.Delay(5000);
+                }
+
+                if (_themeQueue.Count == 1)
                 {
                     await _voiceClient.CloseAsync();
                     _voiceClient.Dispose();
+                    _voiceClient = null;
+
+                    _stream.Dispose();
+                    _stream = null;
+
+                    _currentChannelId = null;
+
+                    await client.UpdateVoiceStateAsync(new VoiceStateProperties(theme.GuildId, null));
                 }
-
+            } catch (Exception)
+            {
+                _themeQueue.Dequeue();
+                _voiceClient?.Dispose();
                 _stream?.Dispose();
-
-                _voiceClient = await client.JoinVoiceChannelAsync(theme.GuildId, theme.ChannelId, new VoiceClientConfiguration
-                {
-                    Logger = new ConsoleLogger()
-                });
-                _currentChannelId = theme.ChannelId;
-
-                await _voiceClient.StartAsync();
-                await Task.Delay(500);
-                await _voiceClient.EnterSpeakingStateAsync(new SpeakingProperties(SpeakingFlags.Microphone));
-
-                var outStream = _voiceClient.CreateOutputStream();
-
-                _stream = new OpusEncodeStream(outStream, PcmFormat.Short, VoiceChannels.Stereo, OpusApplication.Audio);
+                throw;
             }
-
-            ProcessStartInfo startInfo = new("ffmpeg")
-            {
-                RedirectStandardOutput = true,
-            };
-            var arguments = startInfo.ArgumentList;
-
-            // Specify the input
-            arguments.Add("-i");
-            arguments.Add(theme.FilePath);
-
-            // Set the logging level to quiet mode
-            arguments.Add("-loglevel");
-            arguments.Add("-8");
-
-            // Set the number of audio channels to 2 (stereo)
-            arguments.Add("-ac");
-            arguments.Add("2");
-
-            // Set the output format to 16-bit signed little-endian
-            arguments.Add("-f");
-            arguments.Add("s16le");
-
-            // Set the audio sampling rate to 48 kHz
-            arguments.Add("-ar");
-            arguments.Add("48000");
-
-            // Direct the output to stdout
-            arguments.Add("pipe:1");
-
-            // Start the FFmpeg process
-            var ffmpeg = Process.Start(startInfo)!;
-
-            logger.LogInformation("Playing theme: {ThemeFilePath} in Guild: {GuildId}, Channel: {ChannelId}", theme.FilePath, theme.GuildId, theme.ChannelId);
-
-            await ffmpeg.StandardOutput.BaseStream.CopyToAsync(_stream);
-
-            await _stream.FlushAsync();
-
-            if (_themeQueue.Count == 1)
-            {
-                await Task.Delay(5000);
-            }
-
-            if (_themeQueue.Count == 1)
-            {
-                await _voiceClient.CloseAsync();
-                _voiceClient.Dispose();
-                _voiceClient = null;
-
-                _stream.Dispose();
-                _stream = null;
-
-                _currentChannelId = null;
-
-                await client.UpdateVoiceStateAsync(new VoiceStateProperties(theme.GuildId, null));
-            }
-
-            _themeQueue.Dequeue();
         }
     }
 }
